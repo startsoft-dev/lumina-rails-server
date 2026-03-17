9e7db6451031d7b7ca2b25258ebf40b5742b3ef2
Bruno Cipolla <brunocmoreira@live.com>
2026-03-17 15:31:47 -0300

    Refactor HasAutoScope to use lazy scope registration
    Instead of registering a default_scope in the included block (which
    runs on LuminaModel and gets lost during Rails development reloading),
    override default_scopes to lazily discover and register auto scopes
    on the specific model class at query time.

--- lib/lumina/concerns/has_auto_scope.rb
+++ lib/lumina/concerns/has_auto_scope.rb
@@ -26,19 +26,12 @@ module Lumina
   module HasAutoScope
     extend ActiveSupport::Concern
 
-    included do
-      default_scope lambda {
-        model = is_a?(ActiveRecord::Relation) ? self.klass : self
-        if model.respond_to?(:lumina_auto_scope_class)
-          scope_class = model.lumina_auto_scope_class
-          scope_class ? scope_class.apply(all) : all
-        else
-          all
-        end
-      }
-    end
-
     class_methods do
+      def inherited(subclass)
+        super
+        subclass.instance_variable_set(:@_lumina_auto_scope_applied, false)
+      end
+
       def lumina_auto_scope_class
         return @lumina_auto_scope_class if instance_variable_defined?(:@lumina_auto_scope_class)
 
@@ -49,6 +42,20 @@ module Lumina
         result
       end
 
+      # Apply the auto scope as a default_scope on this specific class.
+      # Called lazily on first query to ensure Zeitwerk has loaded the scope class.
+      def apply_lumina_auto_scope!
+        return if @_lumina_auto_scope_applied
+        @_lumina_auto_scope_applied = true
+
+        scope_class = lumina_auto_scope_class
+        if scope_class
+          default_scope lambda {
+            scope_class.apply(where(nil))
+          }
+        end
+      end
+
       private
 
       def find_auto_scope_class
@@ -57,6 +64,17 @@ module Lumina
         model_name = name.demodulize
         "Scopes::#{model_name}Scope".safe_constantize ||
           "ModelScopes::#{model_name}Scope".safe_constantize
+      end
+    end
+
+    included do
+      # Hook into relation building to lazily apply auto scopes.
+      # This ensures scopes are applied after Zeitwerk has loaded all classes.
+      class << self
+        def default_scopes
+          apply_lumina_auto_scope! if respond_to?(:apply_lumina_auto_scope!)
+          super
+        end
       end
     end
   end
